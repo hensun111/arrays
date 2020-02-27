c5b89fbb0193ad29456c47c5e0e88ebe14a97c94
hensun111 <58708806+hensun111@users.noreply.github.com>
2020-02-27 14:01:00 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let arrayLoop: number[] =[]
+
+for (let index = 0; index < 4; index++) {
+
+    arrayLoop[index] = index *2
+
+    game.splash("This multiplies the index times two " + arrayLoop[index])
+} 
